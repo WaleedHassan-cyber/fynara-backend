b37131a3fb85ac73ab2188a89fb4e3ccd104fea2
WaleedHassan-cyber <waleedhassan3722@gmail.com>
2025-09-09 22:36:42 +0500

    s

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.use(
       }
     },
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type"],
     credentials: true,
   })
 );
