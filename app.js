630cffc11af35f73d76b9d852feb3e01733164c2
WaleedHassan-cyber <waleedhassan3722@gmail.com>
2025-09-10 16:31:27 +0500

    sb

--- app.js
+++ app.js
@@ -30,19 +30,22 @@ const PORT = process.env.PORT || 8000;
 // const cors = require("cors");
 
 const allowedOrigins = [
-  "https://shoppii-admin.vercel.app",
+  process.env.CLIENT_URL || "https://shoppii-admin.vercel.app",
   "http://localhost:5173"
 ];
 
 app.use(
   cors({
     origin: function (origin, callback) {
+      console.log("Origin:", origin)
       if (!origin || allowedOrigins.includes(origin)) {
         callback(null, true);
       } else {
         callback(new Error("Not allowed by CORS"));
       }
     },
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type"],
     credentials: true,
   })
 );
